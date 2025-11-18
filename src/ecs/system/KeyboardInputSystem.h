7f1629dd5d00f4aff364558e652b9bc1a3535f22
Dakaro Mueller <LukarioMC@gmail.com>
2025-11-18 11:35:43 -0800

    Fixed separate player inputs (that was broken during merge)

--- src/ecs/system/KeyboardInputSystem.h
+++ src/ecs/system/KeyboardInputSystem.h
@@ -15,27 +15,28 @@
 class KeyboardInputSystem {
 public:
     void update(const std::vector<std::unique_ptr<Entity>>& entities, const SDL_Event& event) {
-        static Vector2D *currentInput = nullptr;
+        static Vector2D *player1Input = nullptr;
+        static Vector2D *player2Input = nullptr;
         for (auto& entity : entities) {
-            if (entity->hasComponent<PlayerTag>() && entity->hasComponent<RigidBody>()) {
+            if (entity->hasComponent<Player1Tag>() && entity->hasComponent<RigidBody>()) {
                 if (event.type == SDL_EVENT_KEY_DOWN) {
                     switch (event.key.key) {
                         case SDLK_W:
-                            currentInput = new Vector2D(0, -1);
+                            player1Input = new Vector2D(0, -1);
                             break;
                         case SDLK_S:
-                            currentInput = new Vector2D(0, 1);
+                            player1Input = new Vector2D(0, 1);
                             break;
                         case SDLK_A:
-                            currentInput = new Vector2D(-1, 0);
+                            player1Input = new Vector2D(-1, 0);
                             break;
                         case SDLK_D:
-                            currentInput = new Vector2D(1, 0);
+                            player1Input = new Vector2D(1, 0);
                             break;
                         default:
                             break;
-                    };
-                };
+                    }
+                }
 
                 if (event.type == SDL_EVENT_KEY_UP) {
                     switch (event.key.key) {
@@ -43,18 +44,53 @@ public:
                         case SDLK_S:
                         case SDLK_A:
                         case SDLK_D:
-                            delete currentInput;
-                            currentInput = nullptr;
+                            delete player1Input;
+                            player1Input = nullptr;
                             break;
                         default:
                             break;
-                    };
-                };
-                if (currentInput)
-                    PhysicsSystem::addImpulse(*entity, *currentInput, 240);
-            };
-        };
-    };
+                    }
+                }
+                if (player1Input)
+                    PhysicsSystem::addImpulse(*entity, *player1Input, 240);
+            } else if (entity->hasComponent<Player2Tag>() && entity->hasComponent<RigidBody>()) {
+                if (event.type == SDL_EVENT_KEY_DOWN) {
+                    switch (event.key.key) {
+                        case SDLK_UP:
+                            player2Input = new Vector2D(0, -1);
+                            break;
+                        case SDLK_DOWN:
+                            player2Input = new Vector2D(0, 1);
+                            break;
+                        case SDLK_LEFT:
+                            player2Input = new Vector2D(-1, 0);
+                            break;
+                        case SDLK_RIGHT:
+                            player2Input = new Vector2D(1, 0);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                if (event.type == SDL_EVENT_KEY_UP) {
+                    switch (event.key.key) {
+                        case SDLK_UP:
+                        case SDLK_DOWN:
+                        case SDLK_LEFT:
+                        case SDLK_RIGHT:
+                            delete player2Input;
+                            player2Input = nullptr;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                if (player2Input)
+                    PhysicsSystem::addImpulse(*entity, *player2Input, 240);
+            }
+        }
+    }
 };
 
 #endif //TUTORIAL1_KEYBOARDINPUTSYSTEM_H
