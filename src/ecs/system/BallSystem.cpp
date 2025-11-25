f39b3e5039dc55d77f9541ca879a35114ff6d90d
Will W <willowhitfield@outlook.com>
2025-11-25 11:20:50 -0800

    Ball cannot be thrown inside other collider

--- src/ecs/system/BallSystem.cpp
+++ src/ecs/system/BallSystem.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "BallSystem.h"
+
+#include "Collision.h"
 #include "World.h"
 #include "Game.h"
 
@@ -20,6 +22,24 @@ void BallSystem::update(World &world, float dt) {
                 b.shouldThrowBall = false;
                 cout << "Checking if ball can be thrown" << endl;
                 if (b.numBallsHeld > 0 && b.cooldown <= 0.0f) {
+                    float ballSprSize = 32;
+                    float middleX = t.position.x + (spr.dst.w / 2) - (ballSprSize/2);
+                    float middleY = t.position.y + (spr.dst.h / 2) - (ballSprSize/2);
+
+                    SDL_FRect dest(middleX + (v.oldDirection.x * ballSpawnDistance), middleY+(v.oldDirection.y * ballSpawnDistance), ballSprSize, ballSprSize );
+
+                    for(auto& entity : world.getEntities()) {
+                        if (entity->hasComponent<Collider>()) {
+                            auto& coll = entity->getComponent<Collider>();
+                            if (coll.tag == "player" || coll.tag == "projectile" || coll.tag == "wall") {
+                                if (Collision::AABB(dest, entity->getComponent<Collider>().rect)) {
+                                    cout << "Too close to other collider" << endl;
+                                    return;
+                                }
+                            }
+                        }
+                    }
+
                     cout << "Throwing ball" << endl;
                     b.numBallsHeld--;
                     b.cooldown = 0.2f;
@@ -27,15 +47,13 @@ void BallSystem::update(World &world, float dt) {
 
                     //create the ball
                     auto &ball(world.createDeferredEntity());
-
                     SDL_Texture *tex = TextureManager::load("../asset/ball.png");
                     SDL_FRect animSrc(0, 0, 32, 32);
-                    SDL_FRect dest(t.position.x, t.position.y, 32, 32);
+
                     ball.addComponent<Sprite>(tex, animSrc, dest);
 
-                    float middleX = t.position.x + (spr.dst.w / 2) - (ball.getComponent<Sprite>().dst.w/2);
-                    float middleY = t.position.y + (spr.dst.h / 2) - (ball.getComponent<Sprite>().dst.h/2);
-                    ball.addComponent<Transform>(Vector2D(middleX+(v.oldDirection.x * ballSpawnDistance), middleY+(v.oldDirection.y * ballSpawnDistance)), 0.0f, 1.0f);
+
+                    ball.addComponent<Transform>(Vector2D(dest.x, dest.y), 0.0f, 1.0f);
                     ball.addComponent<RigidBody>(500.0f, 3.0f);
                     PhysicsSystem::addImpulse(ball, Vector2D(v.oldDirection.x, v.oldDirection.y), 500.0f);
 
